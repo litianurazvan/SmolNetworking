41d764fe770529b6d717e9fd493e2553501789d8
Razvan Litianu <litianu.razvan@gmail.com>
2020-05-02 16:52:42 +0300

    Initial Commit

---
+++ SmolNetworking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmolNetworking
+//
+//  Created by Razvan Litianu on 02/05/2020.
+//  Copyright © 2020 Razvan Litianu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
